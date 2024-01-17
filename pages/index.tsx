03dbc75e4f275ef8418ae7bc00758c235f59c55a
alhen002 <a.henting@posteo.de>
2024-01-17 08:17:19 +0100

    bootstraped the config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import Login from "@/components/Login";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <section>
+        {" "}
+        Signed in as {session?.user?.email} <br />{" "}
+        <button onClick={() => signOut()}>Sign out</button>{" "}
+      </section>
+    );
+  }
   return (
     <section>
-      Hallo
+      {" "}
+      Not signed in <br /> <button onClick={() => signIn()}>
+        Sign in
+      </button>{" "}
       <Login />
     </section>
   );
