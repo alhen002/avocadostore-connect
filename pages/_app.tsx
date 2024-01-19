26cd5deeaf6ea6ab9b2e64a955932b66ab4bed77
Alexander Henting <a.henting@posteo.de>
2024-01-19 17:55:04 +0100

    initialzied react query - first test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,28 @@ import { Inter } from "next/font/google";
 import { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
+// Create a client
+const queryClient = new QueryClient();
+
 export default function App({
   Component,
   pageProps: { session, pageProps },
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <main
-        className={cn(
-          `z-0 mx-auto w-full flex min-h-screen align-center gap-12 flex-col py-12 px-4 md:px-24 bg-neutral-100 dark:bg-neutral-900 transition-colors duration-500`,
-          inter.className
-        )}
-      >
-        <Component {...pageProps} />
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <main
+          className={cn(
+            `z-0 mx-auto w-full flex min-h-screen align-center gap-12 flex-col py-12 px-4 md:px-24 bg-neutral-100 dark:bg-neutral-900 transition-colors duration-500`,
+            inter.className
+          )}
+        >
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,23 +1,34 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { User } from "next-auth";
 import Heading from "@/components/Heading";
 import Paragraph from "@/components/Paragraph";
 import { Button } from "@/components/ui/button";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 function AdminPage() {
   const router = useRouter();
   const { data: session, status } = useSession();
-
+  const queryClient = useQueryClient();
   if (status === "unauthenticated") {
     router.push("/");
   }
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const res = await fetch("/api/user/me");
+      return res.json();
+    },
+  });
 
   async function handleGenerateToken() {
     await fetch("/api/user/token/generate", {
       method: "POST",
     });
+    await queryClient.invalidateQueries({ queryKey: ["user"] });
   }
 
   return (
@@ -26,6 +37,10 @@ function AdminPage() {
       <Paragraph>
         Let us set up your connection. Click on the button underneath to
         generate a secret token, which we will use for a save connection.
+      </Paragraph>
+      <Paragraph>
+        {isLoading && "Is Loading..."}
+        {user?.secret}
       </Paragraph>
       <Button onClick={handleGenerateToken}>Generate Token</Button>
     </div>
